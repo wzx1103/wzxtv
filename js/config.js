3bd8d0e9cb8bf4649514bb211b0839ea6c7f1563
wzx1103 <1461720410@qq.com>
2025-12-10 22:50:35 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -23,7 +23,7 @@ const SITE_CONFIG = {
 // API站点配置
 const API_SITES = {
     dyttzy: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod/at/xml',
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
         detail: 'http://caiji.dyttzyapi.com', 
     },
